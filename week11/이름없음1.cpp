1372984613f6b3185dea88983ba24052d154496a
juha7 <juha140601@gmail.com>
2025-08-23 14:52:38 +0900

    Add files via upload

---
+++ week11/이름없음1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+
+	int i;
+	int j;
+	int sum;
+	
+	
+	for(i=0;i<=10;i++){
+			for(j=1;j<=i;j++){
+				printf("%d ",j );sum +=j;	
+}
+    printf("\n");		
+	printf("%d", sum);
+}
+}
